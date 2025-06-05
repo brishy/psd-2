4f0e19fa230460a35d001a57d98aa18b1097e00d
brishy <vietnemo123@gmail.com>
2025-06-05 21:00:40 +1000

    part 2 almost done

---
+++ build/CMakeFiles/ZOOrk.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/CSE3PSD/ZOOrk/Character.cpp" "CMakeFiles/ZOOrk.dir/Character.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/Character.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/Door.cpp" "CMakeFiles/ZOOrk.dir/Door.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/Door.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/GameObject.cpp" "CMakeFiles/ZOOrk.dir/GameObject.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/GameObject.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/Item.cpp" "CMakeFiles/ZOOrk.dir/Item.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/Item.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/Location.cpp" "CMakeFiles/ZOOrk.dir/Location.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/Location.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/NullCommand.cpp" "CMakeFiles/ZOOrk.dir/NullCommand.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/NullCommand.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/NullPassage.cpp" "CMakeFiles/ZOOrk.dir/NullPassage.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/NullPassage.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/NullRoom.cpp" "CMakeFiles/ZOOrk.dir/NullRoom.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/NullRoom.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/Passage.cpp" "CMakeFiles/ZOOrk.dir/Passage.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/Passage.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/PassageDefaultEnterCommand.cpp" "CMakeFiles/ZOOrk.dir/PassageDefaultEnterCommand.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/PassageDefaultEnterCommand.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/Player.cpp" "CMakeFiles/ZOOrk.dir/Player.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/Player.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/Room.cpp" "CMakeFiles/ZOOrk.dir/Room.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/Room.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/RoomDefaultEnterCommand.cpp" "CMakeFiles/ZOOrk.dir/RoomDefaultEnterCommand.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/RoomDefaultEnterCommand.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/ZOOrkEngine.cpp" "CMakeFiles/ZOOrk.dir/ZOOrkEngine.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/ZOOrkEngine.cpp.obj.d"
+  "D:/CSE3PSD/ZOOrk/main.cpp" "CMakeFiles/ZOOrk.dir/main.cpp.obj" "gcc" "CMakeFiles/ZOOrk.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
